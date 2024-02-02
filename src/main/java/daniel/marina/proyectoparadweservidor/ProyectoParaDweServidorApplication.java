27af772a2235a124252ee4ba3a9b282c080d99c3
Daniel Rodríguez Muñoz <daniel.ro.mu02@gmail.com>
2024-02-02 09:34:36 +0100

    Initial commit

---
+++ src/main/java/daniel/marina/proyectoparadweservidor/ProyectoParaDweServidorApplication.java
@@ -0,0 +1,13 @@
+package daniel.marina.proyectoparadweservidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoParaDweServidorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoParaDweServidorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/daniel/marina/proyectoparadweservidor/ProyectoParaDweServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package daniel.marina.proyectoparadweservidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoParaDweServidorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
