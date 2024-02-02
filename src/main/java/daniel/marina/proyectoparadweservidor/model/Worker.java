7e71a8befcc64f1a89dd62f77fd75ca0cf2e425a
Marina Pintado <darkhuoyt@gmail.com>
2024-02-02 09:52:23 +0100

    Worker class data added

--- src/main/java/daniel/marina/proyectoparadweservidor/model/Worker.java
+++ src/main/java/daniel/marina/proyectoparadweservidor/model/Worker.java
@@ -1,4 +1,18 @@
 package daniel.marina.proyectoparadweservidor.model;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.UUID;
+@Data
+@Table
 public class Worker {
+    @Id
+    private UUID id = null;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private UUID departmentUuid;
 }
